90c634d20dadd1a9332a3f6dcd4a3f1614219494
Ahmet <ahmet@example.com>
2025-01-24 11:45:18 +0300

    static files uploaded

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-const fn = () => {
-    console.log('func worked');
-};
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'))
+
+
+
+app.get('/', (req, res) => {
+res.sendFile(__dirname + '/temp/index.html');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
